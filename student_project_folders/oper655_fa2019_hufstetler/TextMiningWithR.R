fc1164989f74f3b51131ceece1d7dc424428ba52
bjhufstetler <bjhufstetler@gmail.com>
2019-10-27 15:52:03 -0400

    oops, made changes on master

---
+++ student_project_folders/oper655_fa2019_hufstetler/TextMiningWithR.R
@@ -0,0 +1,98 @@
+### Chapter 1
+## Get data ready for tidy
+# Import the data
+text <- c("Because I could not stop for Death -",
+          "He kindly stopped for me -",
+          "The Carriage held but just Ourselves -",
+          "and Immortality")
+# Put it into a dataframe using dplyr
+library(dplyr)
+text_df <- tibble(line = 1:4, text = text)
+
+# Tokenize the data with tidytext
+library(tidytext)
+text_df %>%
+  unnest_tokens(word, text)
+
+
+# Tidy the works of Jane Austen
+library(janeaustenr)
+library(dplyr)
+library(stringr)
+original_books <- austen_books() %>%
+  group_by(book) %>%
+  mutate(linenumber = row_number(),
+         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]",
+                                                 ignore_case = TRUE)))) %>%
+  ungroup()
+
+# Tokenize
+library(tidytext)
+tidy_books <- original_books %>%
+  unnest_tokens(word, text)
+
+# Remove Stop-Words
+data(stop_words)
+tidy_books <- tidy_books %>%
+  anti_join(stop_words)
+
+# Count, sort, and plot with dplyr and ggplot2
+library(ggplot2)
+tidy_books %>%
+  count(word, sort=T) %>%
+  filter(n > 600) %>%
+  mutate(word = reorder(word,n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
+# Gutenbergr Data
+library(gutenbergr)
+hgwells <- gutenberg_download(c(35, 36, 5230, 159))
+tidy_hgwells <- hgwells %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words)
+tidy_hgwells %>%
+  count(word, sort = T)
+
+bronte <- gutenberg_download(c(1260, 768, 969, 9182, 767))
+tidy_bronte <- bronte %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words)
+tidy_bronte %>%
+  count(word, sort = T)
+
+# Compare Austen, Wells, and Bronte
+library(tidyr)
+frequency <- bind_rows(mutate(tidy_bronte, author = "Brontë Sisters"),
+                       mutate(tidy_hgwells, author = "H.G. Wells"),
+                       mutate(tidy_books, author = "Jane Austen")) %>%
+  mutate(word = str_extract(word, "[a-z']+")) %>%
+  count(author, word) %>%
+  group_by(author) %>%
+  mutate(proportion = n / sum(n)) %>%
+  select(-n) %>%
+  spread(author, proportion) %>%
+  gather(author, proportion, `Brontë Sisters`:`H.G. Wells`)
+
+# Visualize tokens
+library(scales)
+ggplot(frequency, aes(x = proportion, y = `Jane Austen`, color = abs(`Jane Austen` - proportion))) +
+  geom_abline(color = "gray40", lty = 2) +
+  geom_jitter(alpha = 0.1, size = 2.5, width = 0.3, height = 0.3) +
+  geom_text(aes(label = word), check_overlap = T, vjust = 1.5) +
+  scale_x_log10(labels = percent_format()) +
+  scale_y_log10(labels = percent_format()) +
+  scale_color_gradient(limits = c(0, 0.001), low = "darkslategray4", high = "gray75") +
+  facet_wrap(~author, ncol = 2) +
+  theme(legend.position = "none") +
+  labs(y = "Jane Austen", x = NULL)
+
+# Test for correlation
+cor.test(data = frequency[frequency$author == "Brontë Sisters",],
+         ~ proportion + `Jane Austen`)
+cor.test(data = frequency[frequency$author == "H.G. Wells",],
+         ~ proportion + `Jane Austen`)
+
+### Chapter 2
