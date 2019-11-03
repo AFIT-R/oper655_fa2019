5fa5a817113e6670db4305a87fd8274d5b155c06
ACGidds <Aaron.Giddings@afit.edu>
2019-11-03 18:10:11 -0500

    Ant-Man and the Wasp Quantum Project Start

---
+++ student_project_folders/oper655_fa2019_giddings/Ant-Man and the Wasp Quantum Project.R
@@ -0,0 +1,68 @@
+# Text Mining Project: MCU Quantum
+# By: Aaron Giddings
+
+pacman::p_load(pdftools,     # extract content from PDF documents
+               XML,          # Working with XML formatted data
+               here,         # References for file paths
+               countrycode,  # Working with names of countries
+               tibble,       # Creating and manipulating tibbles
+               qdap,
+               stringr,      # Tools for qualitative data
+               DT)
+if (packageVersion("devtools") < 1.6) {
+  install.packages("devtools")
+}
+install.packages(qdap)
+library(qdap)
+devtools::install_github("bradleyboehmke/harrypotter")
+harrypotter::philosophers_stone[1]
+pacman::p_load(tm, 
+               pdftools, 
+               here,
+               tau,
+               tidyverse,
+               stringr,
+               tidytext, 
+               RColorBrewer,
+               qdap,
+               qdapRegex,
+               qdapDictionaries,
+               qdapTools,
+               data.table,
+               coreNLP,
+               scales,
+               harrypotter,
+               text2vec,
+               SnowballC,
+               DT,
+               quanteda,
+               RWeka,
+               broom,
+               tokenizers,
+               grid,
+               knitr,
+               widyr)
+pacman::p_load_gh("dgrtwo/drlib",
+                  "trinker/termco", 
+                  "trinker/coreNLPsetup",        
+                  "trinker/tagger")
+pacman::p_load(pdftools,     # extract content from PDF documents
+               XML,          # Working with XML formatted data
+               here,         # References for file paths
+               countrycode,  # Working with names of countries
+               tibble,       # Creating and manipulating tibbles
+               qdap,
+               stringr)      # Tools for qualitative data
+
+library(here)
+root <- here('data', 'pdf_raw','Ant-Man and the Wasp Script')
+
+ant-man_and_the_wasp_pdf <- list.files(root, 
+                           pattern = '17-965\\S+pdf$',
+                           full.names = T)
+
+ant-man_and_the_wasp_pdf 
+tb_pdftools <- pdftools::pdf_text(ant-man_and_the_wasp_pdf)
+
+text_tb <- tibble::tibble(chapter = base::seq_along(philosophers_stone),
+                          text = philosophers_stone)
