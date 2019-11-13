c3e6fcb5e123307d9e997a214d38ac3501aa704f
ACGidds <Aaron.Giddings@afit.edu>
2019-11-13 13:48:51 -0500

    Data files imported after setting up spacyr.

---
+++ student_project_folders/oper655_fa2019_giddings/NER_spacyr_cellphone_reviews.R
@@ -0,0 +1,3 @@
+pur_data2 <- read.csv(file="D:/AFIT/Quarter 5/OPER 655 - Text Mining/phone_user_review_file_2.csv", header=TRUE, sep=",")
+pur_data4 <- read.csv(file="D:/AFIT/Quarter 5/OPER 655 - Text Mining/phone_user_review_file_4.csv", header=TRUE, sep=",")
+pur_data6 <- read.csv(file="D:/AFIT/Quarter 5/OPER 655 - Text Mining/phone_user_review_file_6.csv", header=TRUE, sep=",")
