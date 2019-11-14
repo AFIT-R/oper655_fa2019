398ce8190f78cb926fd1904e5b53c6162925d9ae
ACGidds <Aaron.Giddings@afit.edu>
2019-11-14 10:39:47 -0500

    Using spacyr on chunks 2, 4, & 6 of the dataset.

--- student_project_folders/oper655_fa2019_giddings/NER_spacyr_cellphone_reviews.R
+++ student_project_folders/oper655_fa2019_giddings/NER_spacyr_cellphone_reviews.R
@@ -1,3 +1,17 @@
-pur_data2 <- read.csv(file="D:/AFIT/Quarter 5/OPER 655 - Text Mining/phone_user_review_file_2.csv", header=TRUE, sep=",")
-pur_data4 <- read.csv(file="D:/AFIT/Quarter 5/OPER 655 - Text Mining/phone_user_review_file_4.csv", header=TRUE, sep=",")
-pur_data6 <- read.csv(file="D:/AFIT/Quarter 5/OPER 655 - Text Mining/phone_user_review_file_6.csv", header=TRUE, sep=",")
+pacman::p_load(tidyr,
+               tidytext,
+               tidyverse,
+               textdata,
+               dplyr,
+               stringr,
+               ggplot2,
+               magrittr,
+               wordcloud,
+               reshape2)
+
+pur_data2 <- read.csv(file="D:/AFIT/Quarter 5/OPER 655 - Text Mining/phone_user_review_file_2.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+pur_data4 <- read.csv(file="D:/AFIT/Quarter 5/OPER 655 - Text Mining/phone_user_review_file_4.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+pur_data6 <- read.csv(file="D:/AFIT/Quarter 5/OPER 655 - Text Mining/phone_user_review_file_6.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+parsed_pd2 <- spacy_parse(pur_data2$extract, tag = TRUE, entity = FALSE, lemma = FALSE)
+parsed_pd4 <- spacy_parse(pur_data4$extract, tag = TRUE, entity = FALSE, lemma = FALSE)
+parsed_pd6 <- spacy_parse(pur_data6$extract, tag = TRUE, entity = FALSE, lemma = FALSE)
