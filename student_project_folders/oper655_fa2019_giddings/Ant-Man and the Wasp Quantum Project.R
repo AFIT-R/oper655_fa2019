c95a215bd2acdfdefbfff01fc3e32f7d055baa71
ACGidds <Aaron.Giddings@afit.edu>
2019-12-02 21:36:49 -0500

    Adding the MCU to the corpus.

--- student_project_folders/oper655_fa2019_giddings/Ant-Man and the Wasp Quantum Project.R
+++ student_project_folders/oper655_fa2019_giddings/Ant-Man and the Wasp Quantum Project.R
@@ -74,15 +74,24 @@ mcu_phase1 <- list.files(dest1,
                          full.names = TRUE)
 
 mcu_phase3[1]
-tb_pdftools <- pdftools::pdf_text(pdf = mcu_phase3[1])
+tb_pdftools_mcu3 <- pdftools::pdf_text(pdf = mcu_phase3[1])
+tb_pdftools_mcu2 <- pdftools::pdf_text(pdf = mcu_phase2[1])
+tb_pdftools_mcu1 <- pdftools::pdf_text(pdf = mcu_phase1[1])
 
 <<<<<<< HEAD
 #Attempting to add the rest of the scripts of the MCU into a corpus.
 for (i in 1:8) {
-  nam <- paste("tb_pdftools",i,sep = "")
+  nam <- paste("tb_pdftools_mcu3",i,sep = "")
   assign(nam,pdftools::pdf_text(pdf = mcu_phase3[i]))
 }
-
+for (i in 1:6) {
+  nam <- paste("tb_pdftools_mcu2",i,sep = "")
+  assign(nam,pdftools::pdf_text(pdf = mcu_phase2[i]))
+}
+for (i in 1:5) {
+  nam <- paste("tb_pdftools_mcu1",i,sep = "")
+  assign(nam,pdftools::pdf_text(pdf = mcu_phase1[i]))
+}
 
 =======
 >>>>>>> c1be969ade6a9957ad9607b20d9eda70b65021b0
